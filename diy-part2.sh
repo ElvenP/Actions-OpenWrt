d4a4c00077bf9fd6953fc6df0b85d265c4dc1721
ElvenP <66731105+ElvenP@users.noreply.github.com>
2021-11-22 14:45:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,21 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 拉取 主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
+git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon.git package/luci-theme-atmaterial-ColorIcon
+
+
+#单独添加软件包
+git clone https://github.com/lisaac/luci-app-dockerman package/dockerman
+git clone https://github.com/ElvenP/luci-app-onliner package/onliner
+git clone https://github.com/siwind/luci-app-wolplus package/luci-app-wolplus
+
+# 替换默认主题为 luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
+# 设置默认IP为 
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
